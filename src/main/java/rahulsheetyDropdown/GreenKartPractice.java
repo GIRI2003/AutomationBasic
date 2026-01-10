48caf67e1dd9829cfcb68eb8e87887ef92dac5bb
GIRI2003 <girirohi2@gmail.com>
2026-01-10 21:58:27 +0530

    Greenkcart complete flow

--- src/main/java/rahulsheetyDropdown/GreenKartPractice.java
+++ src/main/java/rahulsheetyDropdown/GreenKartPractice.java
@@ -8,15 +8,20 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.Select;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.Assert;
 
 public class GreenKartPractice {
 
 	public static void main(String[] args) {
 
-		String[] veg = {"Brocolli","Cauliflower","Carrot"};
+		String[] veg = {"Tomato","Beans","Apple"};
 		List<String> vegetableList = Arrays.asList(veg);
 		
 		WebDriver driver = new ChromeDriver();
+		WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(5));
 		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
 		driver.manage().window().maximize();
 		driver.get("https://rahulshettyacademy.com/seleniumPractise/#/");
@@ -29,8 +34,38 @@ public class GreenKartPractice {
 			}
 			i++;
 		}
+		driver.findElement(By.cssSelector("img[alt='Cart']")).click();
+		driver.findElement(By.cssSelector("div[class='action-block'] button")).click();
+		
+		wait.until(ExpectedConditions.visibilityOfAllElementsLocatedBy(
+		        By.xpath("//table/tbody/tr/td[2]")));
+		
+		List<WebElement> addedItems = driver.findElements(By.xpath("//table/tbody/tr/td[2]/p"));
+		for (WebElement addedItem : addedItems) {
+			String addedItemName = addedItem.getText().split("-")[0].trim();
+			System.out.println(addedItemName);
+		}
+		
+		driver.findElement(By.cssSelector(".promoCode")).sendKeys("rahulshettyacademy");
+		driver.findElement(By.cssSelector(".promoBtn")).click();
 		
 		
+		WebElement promoMessage = driver.findElement(By.cssSelector(".promoInfo"));
+		
+		wait.until(ExpectedConditions.visibilityOf(promoMessage));
+		
+		String promoText = driver.findElement(By.cssSelector(".promoInfo")).getText(); 
+		Assert.assertEquals(promoText, "Code applied ..!");
+		driver.findElement(By.xpath("//div/button[text()='Place Order']")).click();
+		
+		
+		WebElement selectCountry = driver.findElement(By.xpath("//div[@class='wrapperTwo']/div/select"));
+		Select dropdown = new Select(selectCountry);
+		dropdown.selectByValue("India");
+		
+		driver.findElement(By.cssSelector("input[type='checkbox']")).click();
+		driver.findElement(By.cssSelector("div[class='wrapperTwo'] button")).click();
+		driver.quit();
 	}
 
 }
